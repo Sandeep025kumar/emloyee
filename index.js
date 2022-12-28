b0c342ef3288657cf92dd5cbc1a491b39e36e18b
sandeep <sandeepvasa025@gamil.com>
2022-12-28 17:00:45 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require('cors');
 const {PORT,DB} = process.env;
 const mongoose = require('mongoose');
 const router = require('./app/routerRout/employee');
@@ -13,6 +14,7 @@ mongoose.connect(DB)
     console.log("Db is not connected",err);
 })
 app.use(express.json());
+app.use(cors());
 app.use('/api',router)
 app.listen(PORT,()=>{
     console.log("Server is running on port:",PORT);
